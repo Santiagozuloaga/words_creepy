09098d697283d3b6563c92a2ecedb4712a94dbca
Santiagozuloaga <118488162+Santiagozuloaga@users.noreply.github.com>
2024-09-21 10:29:47 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "ROFL": "ROFL se utiliza como reacción a algo gracioso, similar a LOL"
+            
+
+
+}
+tank=input("Escribe una palabra que no entiendas.")
+
+if tank in meme_dict. keys():
+print(meme_dict[tank])
+else:
+print("Esa palabra no existe en el diccionario.")
